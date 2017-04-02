800c0c96b5da9eb647d20d882a9cb6076078a879
fewieden <fewieden@htwg-konstanz.de>
2017-04-03 00:48:18 +0200

    outsourced data querying

---
+++ apis/tankerkoenig.js
@@ -0,0 +1,72 @@
+/* Magic Mirror
+ * Module: MMM-Fuel
+ *
+ * By fewieden https://github.com/fewieden/MMM-Fuel
+ * MIT Licensed.
+ */
+
+/* eslint-env node */
+
+const request = require('request');
+
+const baseUrl = 'https://creativecommons.tankerkoenig.de/json/list.php';
+
+module.exports = (config) => {
+    const options = {
+        url: `${baseUrl}?lat=${config.lat}&lng=${config.lng}&rad=${config.radius}&type=all&apikey=${
+            config.api_key}&sort=dist`
+    };
+
+    return {
+        getData(callback) {
+            request(options, (error, response, body) => {
+                if (response.statusCode === 200) {
+                    const parsedBody = JSON.parse(body);
+                    if (parsedBody.ok) {
+                        const stations = parsedBody.stations.filter((element) => {
+                            for (let i = 0; i < config.types.length; i += 1) {
+                                if (element[config.types[i]] <= 0 || (config.showOpenOnly && !element.isOpen)) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        });
+
+                        stations.forEach((value, index) => {
+                            stations[index].prices = {
+                                diesel: value.diesel,
+                                e5: value.e5,
+                                e10: value.e10
+                            };
+                            stations[index].distance = value.dist;
+                            stations[index].address = `${(`0${value.postCode}`).slice(-5)} ${
+                                value.place} - ${value.street} ${value.houseNumber}`;
+                        });
+
+                        const price = stations.slice(0);
+                        price.sort((a, b) => {
+                            if (b[config.sortBy] === 0) {
+                                return Number.MIN_SAFE_INTEGER;
+                            } else if (a[config.sortBy] === 0) {
+                                return Number.MAX_SAFE_INTEGER;
+                            }
+                            return a[config.sortBy] - b[config.sortBy];
+                        });
+
+                        callback(null, {
+                            types: ['diesel', 'e5', 'e10'],
+                            unit: 'km',
+                            currency: 'EUR',
+                            byPrice: price,
+                            byDistance: stations
+                        });
+                    } else {
+                        callback('Error no fuel data');
+                    }
+                } else {
+                    callback(`Error getting fuel data ${response.statusCode}`);
+                }
+            });
+        }
+    };
+};
--- node_helper.js
+++ node_helper.js
@@ -8,64 +8,37 @@
 /* eslint-env node */
 /* eslint-disable no-console */
 
-const request = require('request');
 const NodeHelper = require('node_helper');
+const fs = require('fs');
 
 module.exports = NodeHelper.create({
 
-    baseUrl: 'https://creativecommons.tankerkoenig.de/json/list.php',
-
     start() {
-        console.log(`Starting module: ${this.name}`);
+        console.log(`Starting module helper: ${this.name}`);
     },
 
     socketNotificationReceived(notification, payload) {
         if (notification === 'CONFIG') {
             this.config = payload;
-            this.getData();
-            setInterval(() => {
+            if (fs.existsSync(`modules/${this.name}/apis/${this.config.provider}.js`)) {
+                // eslint-disable-next-line global-require, import/no-dynamic-require
+                this.provider = require(`./apis/${this.config.provider}`)(this.config);
                 this.getData();
-            }, this.config.updateInterval);
+                setInterval(() => {
+                    this.getData();
+                }, this.config.updateInterval);
+            } else {
+                console.log(`${this.name}: Couldn't load provider ${this.config.provider}`);
+            }
         }
     },
 
     getData() {
-        const options = {
-            url: `${this.baseUrl}?lat=${this.config.lat}&lng=${this.config.lng}&rad=${this.config.radius
-                }&type=all&apikey=${this.config.api_key}&sort=dist`
-        };
-        request(options, (error, response, body) => {
-            if (response.statusCode === 200) {
-                const parsedBody = JSON.parse(body);
-                if (parsedBody.ok) {
-                    for (let i = parsedBody.stations.length - 1; i >= 0; i -= 1) {
-                        let removeFlag = false;
-                        for (let n = 0; n < this.config.types.length; n += 1) {
-                            if (parsedBody.stations[i][this.config.types[n]] <= 0 ||
-                                (this.config.showOpenOnly && !parsedBody.stations[i].isOpen)) {
-                                removeFlag = true;
-                                break;
-                            }
-                        }
-                        if (removeFlag) {
-                            parsedBody.stations.splice(i, 1);
-                        }
-                    }
-                    const price = parsedBody.stations.slice(0);
-                    price.sort((a, b) => {
-                        if (b[this.config.sortBy] === 0) {
-                            return Number.MIN_SAFE_INTEGER;
-                        } else if (a[this.config.sortBy] === 0) {
-                            return Number.MAX_SAFE_INTEGER;
-                        }
-                        return a[this.config.sortBy] - b[this.config.sortBy];
-                    });
-                    this.sendSocketNotification('PRICELIST', { byPrice: price, byDistance: parsedBody.stations });
-                } else {
-                    console.log('Error no fuel data');
-                }
+        this.provider.getData((err, data) => {
+            if (err) {
+                console.log(err);
             } else {
-                console.log(`Error getting fuel data ${response.statusCode}`);
+                this.sendSocketNotification('PRICELIST', data);
             }
         });
     }
